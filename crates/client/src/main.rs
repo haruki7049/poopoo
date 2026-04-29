67dd6789fdf24a91df28bc8716392d036c0c475a
haruki7049 <tontonkirikiri@gmail.com>
2026-04-29 18:38:06 +0900

    Update

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Poopoo",
+        native_options,
+        Box::new(|cc| Ok(Box::new(PoopooApp::new(cc)))),
+    )?;
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct PoopooApp {}
+
+impl PoopooApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_global_style.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for PoopooApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
 }
